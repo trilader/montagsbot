969e3a36c321ea84547af559e2a10e6be228ed8d
Daniel Schulte <daniel@schultes-home.de>
2016-04-21 15:47:05 +0200

    Added mailpipe script
    This is the other end of the RPC pipe to deliver mail to the channel

---
+++ mailpipe.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+import xmlrpc.client
+
+if __name__ == "__main__":
+    s=""
+    for line in sys.stdin:
+        s+=line
+
+    print("Mail is:",s)
+
+    remote = xmlrpc.client.ServerProxy("http://localhost:4711")
+    try:
+        remote.handle_reply_mail(s)
+    except xmlrpc.client.Fault as err:
+        print("A fault occurred")
+        print("Fault code: %d" % err.faultCode)
+        print("Fault string: %s" % err.faultString)
+    except Exception as ex:
+        print("Error:",ex)
